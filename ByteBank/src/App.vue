1e0f07e6e07b3543c3abab6f005e1ae3045cc610
Natalie <nataliegf2014@gmail.com>
2022-03-24 19:15:52 -0300

    raiz projeto

---
+++ ByteBank/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <navBar />
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+import navBar from '@/components/NavBar'
+
+export default {
+  
+  components : {
+    navBar
+  }
+
+};
+</script>
+
